b6685c911aad0279cb1ad8179131682bcd83b42d
Noe G Mena <nmgonzalez94@gmail.com>
2024-05-08 12:54:13 -0600

    sustitución de boton por checkbox

--- app.py
+++ app.py
@@ -6,23 +6,18 @@ car_data = pd.read_csv("C:/Users/noego/Documents/TripleTen/Sprint_5/Proyecto_de_
 
 st.header("Venta de carros")
 
-hist_button = st.button('Construir Histograma')
-
-if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    #Creación del histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
-
-
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
+build_dispersion = st.checkbox('Construir un diagrama de dispersion')
 
 if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
+    st.write('Histograma para la columna odómetro')
     fig_check=px.histogram(car_data, x="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_check, use_container_width=True)
+
+if build_dispersion: # si la casilla de verificación está seleccionada
+    st.write('Diagrama de dispersion: odómetro VS price')
+    fig_check=px.scatter(car_data, x="odometer", y="price")
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig_check, use_container_width=True)
