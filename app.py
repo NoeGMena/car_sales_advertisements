c9dd4a8e338c838339a29a141e27dc3acc4dcaa3
Noe G Mena <nmgonzalez94@gmail.com>
2024-05-08 14:01:24 -0600

    ruta relativa

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-car_data = pd.read_csv("C:/Users/noego/Documents/TripleTen/Sprint_5/Proyecto_de_sprint/car_sales_advertisements/vehicles_us.csv") # leer los datos
+car_data = pd.read_csv("vehicles_us.csv") # leer los datos
 
 st.header("Venta de carros")
 
